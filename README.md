7c71a85266464431663da27b2499a7c81136ebd0
Ashwin Saravanapavan <ashwinsiva13@gmail.com>
2025-09-02 14:18:58 -0400

    basic information of mp3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mp3
-
+## source /Users/ashwinsaravanapavan/Document/personal_project/mp3/.venv/bin/activate
+## python app.py
--- app.py
+++ app.py
@@ -2,28 +2,32 @@ from flask import Flask, request, render_template
 import os
 from audio_utils import analyze_mp3, evaluate_quality
 
-#by default it looks for templates in a folder named 'templates'
+#By default it looks for templates in a folder named 'templates'
 app = Flask(__name__, template_folder='frontend')
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return "No file part", 400
-        file = request.files['file']
-        if file.filename == '':
-            return "No selected file", 400
-        if not file.filename.lower().endswith('.mp3'):
-            return "Only MP3 files are allowed", 400
+    return render_template("index.html")
+
+@app.route('/analyse', methods=['POST'])
+def analyse():
+    if 'file' not in request.files:
+        return "No file part", 400
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file", 400
+    if not file.filename.lower().endswith('.mp3'):
+        return "Only MP3 files are allowed", 400
         
-        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
-        file.save(file_path)
-        metrics = analyze_mp3(file_path)
-        quality = evaluate_quality(metrics)
-        return render_template('results.html', metrics=metrics, quality=quality)
-    return render_template('index.html', results=None)
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(file_path)
+    metrics = analyze_mp3(file_path)
+    quality = evaluate_quality(metrics)
+    print(metrics)
+    print(quality)
+    return render_template('results.html', metrics=metrics, quality=quality)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
